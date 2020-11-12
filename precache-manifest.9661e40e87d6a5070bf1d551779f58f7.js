fbdd72184b6151ad0f9dc74bf4d99a469e6d8046
khush7750 <meherkhushbant@gmail.com>
2020-11-12 23:22:17 +0530

    Updates

---
+++ precache-manifest.9661e40e87d6a5070bf1d551779f58f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d18df56b9260cd81b55c597bde4245",
+    "url": "/Google-Keep-Clone/index.html"
+  },
+  {
+    "revision": "62ac990383425ace1089",
+    "url": "/Google-Keep-Clone/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d51fbe6cf210174a3426",
+    "url": "/Google-Keep-Clone/static/css/main.70ee5f5d.chunk.css"
+  },
+  {
+    "revision": "62ac990383425ace1089",
+    "url": "/Google-Keep-Clone/static/js/2.33b03076.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Google-Keep-Clone/static/js/2.33b03076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51fbe6cf210174a3426",
+    "url": "/Google-Keep-Clone/static/js/main.89b0e502.chunk.js"
+  },
+  {
+    "revision": "46bdf137d74bda364be0",
+    "url": "/Google-Keep-Clone/static/js/runtime-main.43ffbbad.js"
+  }
+]);
